6ad82e8e2cc17fd98b49b9b6e454278a747c0c52
Freedomcoding101 <97307133+Freedomcoding101@users.noreply.github.com>
2024-05-15 14:15:09 +0800

    working on sending out emails to users as well as password reset

--- users/urls.py
+++ users/urls.py
@@ -6,11 +6,14 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerUser, name="register"),
     path('', views.profiles, name="profiles"),
-    path('profile/<str:pk>', views.userProfile, name='user-profile'),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
     path('account/', views.userAccount, name='account'),
     path('edit_account/', views.editAccount, name='edit-account'),
     path('create-skill/', views.createSkill, name="create-skill"),
     path('edit-skill/<str:pk>/', views.editSkill, name="edit-skill"),
-    path('delete-skill/<str:pk>/', views.deleteSkill, name="delete-skill")
+    path('delete-skill/<str:pk>/', views.deleteSkill, name="delete-skill"),
+    path('inbox/', views.inbox, name='inbox'),
+    path('message/<str:pk>/', views.viewMessage, name='message'),
+    path('create-message/<str:pk>/', views.createMessage, name='create-message'),
     
 ]
