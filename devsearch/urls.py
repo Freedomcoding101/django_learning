172df615673de422681763b30e1860e1e9a7135a
Freedomcoding101 <97307133+Freedomcoding101@users.noreply.github.com>
2024-05-15 18:04:41 +0800

    finished emailing new users and password reset

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -12,13 +12,13 @@ urlpatterns = [
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"),
     name="reset_password"),
 
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"),
     name="password_reset_done"),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
     name="password_reset_confirm"),
 
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
     name="password_reset_complete"),
 ]
 
